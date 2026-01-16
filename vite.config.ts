88d5c3de3de8acf3385dc9daf3e2326bae2caabd
SJang1 <siliconsjang@gmail.com>
2026-01-16 23:51:52 +0900

    logo and PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { cloudflare } from "@cloudflare/vite-plugin"
 
@@ -11,7 +12,36 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		vueDevTools(),
-		cloudflare()
+		cloudflare(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
+			manifest: {
+				name: '코레일 승무원 정보',
+				short_name: '코레일 승무원',
+				description: '코레일 승무원 근무 정보 확인 웹사이트',
+				theme_color: '#1976d2',
+				icons: [
+					{
+						src: 'logo.png',
+						sizes: '50x50',
+						type: 'image/png',
+					}
+					{
+						src: 'logo.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'logo.png',
+						sizes: '512x512',
+						type: 'image/svg+xml',
+					}
+				]
+			}
+		})
 	],
 	resolve: {
 		alias: {
